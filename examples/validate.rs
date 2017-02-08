badfdf4d9bce548c926fc89cf6b6be83c07c22f7
Harald Hoyer <harald@redhat.com>
2017-02-08 08:44:11 +0100

    add validate example

---
+++ examples/validate.rs
@@ -0,0 +1,30 @@
+extern crate varlink;
+
+use std::io;
+use std::io::prelude::*;
+use varlink::parser::Varlink;
+use std::process::exit;
+
+fn main() {
+    let mut buffer = String::new();
+    if let Err(e) = io::stdin().read_to_string(&mut buffer) {
+        println!("Error {:?}", e);
+        exit(1);
+    }
+
+    match Varlink::from_string(&buffer) {
+        Ok(v) => {
+            println!("Syntax check passed!");
+            println!("Interfaces:");
+            for k in v.interfaces.keys() {
+                println!("\t{}", k);
+            }
+            exit(0);
+        }
+        Err(e) => {
+            println!("{}", e);
+            exit(1);
+        }
+    };
+
+}

4c1a53995bce713653ccd08a2deeac3ea4d1fff3
Harald Hoyer <harald@redhat.com>
2018-03-16 17:10:19 +0100

    add exec: activation

--- varlink/src/server.rs
+++ varlink/src/server.rs
@@ -3,7 +3,12 @@ use std::io::{Error, ErrorKind, Read, Write};
 use std::thread;
 use std::sync::Arc;
 use std::net::{TcpListener, TcpStream};
+// FIXME: abstract unix domains sockets still not in std
+// FIXME: https://github.com/rust-lang/rust/issues/14194
 use unix_socket::{UnixListener, UnixStream};
+use std::fs;
+use std::env;
+use std::os::unix::io::FromRawFd;
 
 enum VarlinkListener {
     TCP(TcpListener),
@@ -28,20 +33,63 @@ impl<'a> VarlinkStream {
     }
 }
 
+fn activation_listener() -> io::Result<Option<UnixListener>> {
+    /*
+	FIXME: only working on nightly https://github.com/rust-lang/rust/pull/45059
+
+    let spid = env::var("LISTEN_PID");
+    if let Ok(pid) = spid {
+        let mypid = std::process::id();
+        match pid.parse::<u32>() {
+            mypid => {}
+            _ => return Ok(None),
+        }
+    } else {
+        return Ok(None);
+    }
+*/
+    let snfds = env::var("LISTEN_FDS");
+    if let Ok(nfds) = snfds {
+        match nfds.parse::<u32>() {
+            Ok(1) => {}
+            _ => return Ok(None),
+        }
+    } else {
+        return Ok(None);
+    }
+    unsafe {
+        //syscall.CloseOnExec(3)
+        let listener = UnixListener::from_raw_fd(3);
+        env::remove_var("LISTEN_PID");
+        env::remove_var("LISTEN_FDS");
+
+        Ok(Some(listener))
+    }
+}
+
+//FIXME: add Drop with shutdown() and unix file removal
 impl VarlinkListener {
     pub fn new(address: &str) -> io::Result<Self> {
+        if let Some(l) = activation_listener()? {
+            return Ok(VarlinkListener::UNIX(l));
+        }
+
         if address.starts_with("tcp:") {
             Ok(VarlinkListener::TCP(TcpListener::bind(&address[4..])?))
         } else if address.starts_with("unix:") {
-            let mut addr = String::from(&address[5..]);
+            let mut addr = String::from(address[5..].split(";").next().unwrap());
             if addr.starts_with("@") {
                 addr = addr.replacen("@", "\0", 1);
+            } else {
+                // ignore error on non-existant file
+                let _ = fs::remove_file(addr.clone());
             }
             Ok(VarlinkListener::UNIX(UnixListener::bind(addr)?))
         } else {
             Err(Error::new(ErrorKind::Other, "unknown varlink address"))
         }
     }
+
     pub fn accept(&self) -> io::Result<VarlinkStream> {
         match self {
             &VarlinkListener::TCP(ref l) => {
@@ -61,7 +109,14 @@ pub fn listen(addr: &str, service: Arc<::VarlinkService>) -> io::Result<()> {
     let listener = VarlinkListener::new(addr)?;
 
     loop {
-        let mut stream = listener.accept()?;
+        let mut stream;
+        match listener.accept() {
+            Err(e) => match Error::last_os_error().raw_os_error() {
+                Some(11) => continue,
+                _ => return Err(e),
+            },
+            Ok(s) => stream = s,
+        }
         let service = service.clone();
         let _join = thread::spawn(move || -> io::Result<()> {
             let (mut r, mut w) = stream.split()?;

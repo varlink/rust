71631e7409de755faa88b8874401e236f03d468b
Harald Hoyer <harald@redhat.com>
2018-03-23 12:38:39 +0100

    support input from stdin if arg[1] is "-"

--- varlink/src/bin/varlink-rust-generator.rs
+++ varlink/src/bin/varlink-rust-generator.rs
@@ -1,7 +1,8 @@
 //! varlink-rust-generator generates rust code from a varlink interface
 //! definition file
 //!
-//! Usage: varlink-rust-generator `[<varlink_file>]`
+//!# Usage
+//! $ varlink-rust-generator `[<varlink_file>]`
 //!
 //! If <varlink_file> is omitted, input is expected to come from stdin.
 //!
@@ -36,14 +37,18 @@ impl<E: Error> MainReturn for Result<(), E> {
 
 fn do_main() -> io::Result<()> {
     let args: Vec<_> = env::args().collect();
-    {
-        let mut reader: Box<Read> = match args.len() {
-            0 | 1 => Box::new(io::stdin()),
-            _ => Box::new(File::open(Path::new(&args[1]))?),
-        };
-        let writer: &mut Write = &mut io::stdout();
-        generate(&mut reader, writer)
-    }
+    let mut reader: Box<Read> = match args.len() {
+        0 | 1 => Box::new(io::stdin()),
+        _ => {
+            if args[1] == "-" {
+                Box::new(io::stdin())
+            } else {
+                Box::new(File::open(Path::new(&args[1]))?)
+            }
+        }
+    };
+    let writer: &mut Write = &mut io::stdout();
+    generate(&mut reader, writer)
 }
 
 fn main() {

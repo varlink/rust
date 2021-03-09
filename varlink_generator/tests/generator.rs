0c8a42dd2156128d47e270615762de46d61850d8
Harald Hoyer <harald@redhat.com>
2021-03-09 10:43:17 +0100

    fix(generator): normalize the output
    Normalize the output with the same `rustfmt` before comparing stuff.
    
    Fixes: https://github.com/varlink/rust/issues/49

--- varlink_generator/tests/generator.rs
+++ varlink_generator/tests/generator.rs
@@ -1,41 +1,78 @@
 extern crate varlink_generator;
 
+use std::fs;
+use std::io::prelude::*;
+use std::process::Command;
+use tempdir::TempDir;
+use unified_diff::diff;
+
 #[cfg(unix)]
 #[test]
 fn test_generate() {
-    use std::fs::File;
-    use std::io::prelude::*;
-    // use std::process::Command;
-
     fn compare_files(a: std::path::PathBuf, b: std::path::PathBuf) -> bool {
-        let fa = File::open(a).unwrap();
-        let fb = File::open(b).unwrap();
-        fa.bytes()
-            .zip(fb.bytes())
+        let from_content = fs::read(a.clone()).unwrap();
+        let to_content = fs::read(b.clone()).unwrap();
+
+        if !from_content
+            .bytes()
+            .zip(to_content.bytes())
             .all(|(a, b)| a.unwrap() == b.unwrap())
+        {
+            std::io::stdout()
+                .write_all(&diff(
+                    &from_content,
+                    &a.to_string_lossy(),
+                    &to_content,
+                    &b.to_string_lossy(),
+                    1,
+                ))
+                .unwrap();
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    std::env::set_var("OUT_DIR", std::env::temp_dir());
+    let tmpdir = TempDir::new("test_generate").unwrap();
+
+    std::env::set_var("OUT_DIR", tmpdir.path().as_os_str());
     varlink_generator::cargo_build("tests/org.example.complex.varlink");
-    let mut path = std::path::PathBuf::from(std::env::temp_dir());
+    let mut path = tmpdir.path().to_owned();
     path.push("org.example.complex.rs");
 
-    // assert!(
-    // Command::new("rustfmt")
-    // .arg(path.to_str().unwrap())
-    // .output()
-    // .is_ok()
-    // );
-    //
-    if !compare_files(
+    let status = Command::new("rustfmt")
+        .arg("--emit")
+        .arg("files")
+        .arg(path.to_str().unwrap())
+        .status()
+        .unwrap();
+
+    assert!(status.success());
+
+    let path2 = path.with_extension("orig");
+
+    assert!(std::fs::copy(
         std::path::PathBuf::from("tests/org.example.complex.rs_out"),
-        path.clone(),
-    ) {
+        path2.clone(),
+    )
+    .is_ok());
+
+    let status = Command::new("rustfmt")
+        .arg("--emit")
+        .arg("files")
+        .arg(path2.to_str().unwrap())
+        .status()
+        .unwrap();
+
+    assert!(status.success());
+
+    if !compare_files(path2.clone(), path.clone()) {
         panic!(
             "Generated files differ: '{}' versus '{}'",
-            "tests/org.example.complex.rs_out",
-            &path.to_str().unwrap()
+            &path.to_str().unwrap(),
+            &path2.to_str().unwrap()
         );
     }
     let _ = std::fs::remove_file(path);
+    let _ = std::fs::remove_file(path2);
 }

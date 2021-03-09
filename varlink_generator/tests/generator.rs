3ae21362b1761973ec1ac135198d3a20455d74e3
Harald Hoyer <harald@redhat.com>
2021-03-09 10:43:17 +0100

    fix(generator): normalize the output
    Normalize the output with the same `rustfmt` before comparing stuff.
    
    Fixes: https://github.com/varlink/rust/issues/49

--- varlink_generator/tests/generator.rs
+++ varlink_generator/tests/generator.rs
@@ -1,12 +1,12 @@
 extern crate varlink_generator;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
+
 #[cfg(unix)]
 #[test]
 fn test_generate() {
-    use std::fs::File;
-    use std::io::prelude::*;
-    // use std::process::Command;
-
     fn compare_files(a: std::path::PathBuf, b: std::path::PathBuf) -> bool {
         let fa = File::open(a).unwrap();
         let fb = File::open(b).unwrap();
@@ -20,22 +20,31 @@ fn test_generate() {
     let mut path = std::path::PathBuf::from(std::env::temp_dir());
     path.push("org.example.complex.rs");
 
-    // assert!(
-    // Command::new("rustfmt")
-    // .arg(path.to_str().unwrap())
-    // .output()
-    // .is_ok()
-    // );
-    //
-    if !compare_files(
+    assert!(Command::new("rustfmt")
+        .arg(path.to_str().unwrap())
+        .output()
+        .is_ok());
+
+    let path2 = path.with_extension("orig");
+
+    assert!(std::fs::copy(
         std::path::PathBuf::from("tests/org.example.complex.rs_out"),
-        path.clone(),
-    ) {
+        path2.clone(),
+    )
+    .is_ok());
+
+    assert!(Command::new("rustfmt")
+        .arg(path2.clone().to_str().unwrap())
+        .output()
+        .is_ok());
+
+    if !compare_files(path2.clone(), path.clone()) {
         panic!(
             "Generated files differ: '{}' versus '{}'",
-            "tests/org.example.complex.rs_out",
-            &path.to_str().unwrap()
+            &path.to_str().unwrap(),
+            &path2.to_str().unwrap()
         );
     }
     let _ = std::fs::remove_file(path);
+    let _ = std::fs::remove_file(path2);
 }

8a3da76a5f1dcf68b9ff9a88f52d9f1014a03829
Harald Hoyer <harald@hoyer.xyz>
2025-10-20 14:12:40 +0200

    feat: add Nix flake support
    Introduce `flake.nix`, `flake.lock`, and `.envrc` to the project, enabling development environments with Nix flakes.

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "varlink - rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rustfmt" "clippy" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            cargo
+            rustc
+            cargo-release
+            rustfmt
+          ];
+
+          shellHook = ''
+            echo "Welcome to varlink - rust"
+            echo "Rust version: $(rustc --version)"
+            echo "Cargo version: $(cargo --version)"
+          '';
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "varlink-rust";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      });
+}
